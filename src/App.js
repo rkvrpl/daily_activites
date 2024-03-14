e12a68fb1ff039bf102db23b011917c496c7c976
rkvrpl <rkvrpl@icloud.com>
2024-03-14 16:56:32 +0300

    done hw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
 import './App.css';
+import React from "react";
+import { Chart } from "react-google-charts";
 
-function App() {
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Работа", 6],
+  ["Еда", 2],
+  ["Учеба", 4],
+  ["Отдых", 3],
+  ["Сон", 9],
+];
+
+export const options = {
+  title: "Мой план на день",
+  is3D: true,
+};
+
+export function App() {
   return (
-    <div className="App">
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"500px"}
+    />
   );
 }
 
